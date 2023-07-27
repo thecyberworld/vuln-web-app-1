dd414c8e9091c016e1df418e8efd2c9619352520
Samellufy <100872010+Samellufy@users.noreply.github.com>
2023-07-27 14:27:05 +0530

    fixed: xss vulnerability

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -3,6 +3,7 @@ import {useParams} from "react-router-dom";
 import {formatISO9075} from "date-fns";
 import {UserContext} from "../UserContext";
 import {Link} from 'react-router-dom';
+import DOMPurify from 'dompurify';
 
 export default function PostPage() {
   const [postInfo,setPostInfo] = useState(null);
@@ -37,7 +38,7 @@ export default function PostPage() {
       <div className="image">
         <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
       </div>
-      <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
+      <div className="content" dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(postInfo.content)}} />
     </div>
   );
 }
