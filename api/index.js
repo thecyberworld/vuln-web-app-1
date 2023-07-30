2cd90b76510bc686374d807ea221b4faa17885ba
sohamsharma966 <sohamsharma966@gmail.com>
2023-07-30 20:13:35 +0530

    no vulnerability

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import helmet from 'helmet'
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
@@ -5,23 +6,45 @@ const User = require('./models/User');
 const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 const app = express();
+app.use(helmet())
+const rateLimit = require('express-rate-limit')
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
+const crypto = require('crypto');
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
 
+function generateRandomPassword(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[{]};:,<.>?';
+  const charactersLength = characters.length;
+  let randomPassword = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = crypto.randomInt(0, charactersLength);
+    randomPassword += characters.charAt(randomIndex);
+  }
+
+  return randomPassword;
+}
 
 
 const salt = bcrypt.genSaltSync(10);
-const secret = 'asdfe45we45w345wegw345werjktjwertkj';
-
+const mypwd = generateRandomPassword(24); // Generate a 24-character random password
+console.log(mypwd);
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 mongoose.connect('mongodb+srv://blog:RD8paskYC8Ayj09u@cluster0.pflplid.mongodb.net/?retryWrites=true&w=majority');
+
 
 app.post('/register', async (req,res) => {
   const {username,password} = req.body;
@@ -43,7 +66,7 @@ app.post('/login', async (req,res) => {
   const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
     // logged in
-    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+    jwt.sign({username,id:userDoc._id}, mypwd, {}, (err,token) => {
       if (err) throw err;
       res.cookie('token', token).json({
         id:userDoc._id,
@@ -57,7 +80,7 @@ app.post('/login', async (req,res) => {
 
 app.get('/profile', (req,res) => {
   const {token} = req.cookies;
-  jwt.verify(token, secret, {}, (err,info) => {
+  jwt.verify(token, mypwd, {}, (err,info) => {
     if (err) throw err;
     res.json(info);
   });
@@ -67,15 +90,19 @@ app.post('/logout', (req,res) => {
   res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
-  const {originalname,path} = req.file;
-  const parts = originalname.split('.');
-  const ext = parts[parts.length - 1];
-  const newPath = path+'.'+ext;
+app.post('/post',limiter ,uploadMiddleware.single('file'), async (req,res) => {
+  const {originalname,path: originalPath} = req.file;
+  // Extract the file extension from the originalname using path.extname()
+  const ext = path.extname(originalname);
+
+  // Generate a unique filename using UUID and the file's original extension
+  const newFileName = `${uuidv4()}${ext}`;
+  const newPath = path.join('uploads', newFileName);
+
   fs.renameSync(path, newPath);
 
   const {token} = req.cookies;
-  jwt.verify(token, secret, {}, async (err,info) => {
+  jwt.verify(token, mypwd, {}, async (err,info) => {
     if (err) throw err;
     const {title,summary,content} = req.body;
     const postDoc = await Post.create({
@@ -90,18 +117,22 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
 
 });
 
-app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+app.put('/post',limiter ,uploadMiddleware.single('file'), async (req,res) => {
   let newPath = null;
   if (req.file) {
-    const {originalname,path} = req.file;
-    const parts = originalname.split('.');
-    const ext = parts[parts.length - 1];
-    newPath = path+'.'+ext;
+    const {originalname,path: originalPath} = req.file;
+    // Extract the file extension from the originalname using path.extname()
+    const ext = path.extname(originalname);
+
+    // Generate a unique filename using UUID and the file's original extension
+    const newFileName = `${uuidv4()}${ext}`;
+    const newPath = path.join('uploads', newFileName);
+
     fs.renameSync(path, newPath);
   }
 
   const {token} = req.cookies;
-  jwt.verify(token, secret, {}, async (err,info) => {
+  jwt.verify(token, mypwd, {}, async (err,info) => {
     if (err) throw err;
     const {id,title,summary,content} = req.body;
     const postDoc = await Post.findById(id);
@@ -136,5 +167,9 @@ app.get('/post/:id', async (req, res) => {
   res.json(postDoc);
 })
 
-app.listen(4000);
+app.use(morgan('dev'));
+require('./routes/currency.route.js')(app);
+app.use(csrf({ cookie: true }));
+
+app.listen(3000);
 //
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -5,19 +5,19 @@ import {UserContext} from "../UserContext";
 import {Link} from 'react-router-dom';
 
 export default function PostPage() {
-  const [postInfo,setPostInfo] = useState(null);
-  const {userInfo} = useContext(UserContext);
-  const {id} = useParams();
+  const [postInfo, setPostInfo] = useState(null);
+  const { userInfo } = useContext(UserContext);
+  const { id } = useParams();
+
   useEffect(() => {
     fetch(`http://localhost:4000/post/${id}`)
-      .then(response => {
-        response.json().then(postInfo => {
-          setPostInfo(postInfo);
-        });
+      .then(response => response.json())
+      .then(postInfo => {
+        setPostInfo(postInfo);
       });
   }, []);
 
-  if (!postInfo) return '';
+  if (!postInfo) return null;
 
   return (
     <div className="post-page">
@@ -35,9 +35,9 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
+        <img src={`http://localhost:4000/${postInfo.cover}`} alt="" />
       </div>
-      <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
+      <div className="content">{postInfo.content}</div>
     </div>
   );
 }
