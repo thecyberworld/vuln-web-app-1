16cd39dff4efeb7c1223ca403dbd94ff8bfeb165
Shubham <vishu.bhardwaz2001@gmail.com>
2024-03-05 23:11:51 +0530

    "2nd change done"

--- api/index.js
+++ api/index.js
@@ -56,7 +56,7 @@ app.post('/login', async (req,res) => {
 
 app.get('/profile', (req,res) => {
   const {token} = req.cookies;
-  jwt.verify(token,processsecret, {}, (err,info) => {
+  jwt.verify(token,secret, {}, (err,info) => {
     if (err) throw err;
     res.json(info);
   });
